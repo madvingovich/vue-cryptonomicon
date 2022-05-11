4f248c6c2ae8b30b74dda331b9333833370043c2
Вадим Логвинович <vadvoker@gmail.com>
2022-05-11 17:01:05 +0300

    add ticker component

---
+++ src/AddTicker.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="max-w-xs">
+    <label for="wallet" class="block text-sm font-medium text-gray-700"
+      >Тикер</label
+    >
+    <div class="mt-1 relative rounded-md shadow-md">
+      <input
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        @keydown.enter="addTicker"
+        @blur="$emit('blur')"
+        type="text"
+        name="wallet"
+        id="wallet"
+        class="
+          block
+          w-full
+          pr-10
+          border-gray-300
+          text-gray-900
+          focus:outline-none focus:ring-gray-500 focus:border-gray-500
+          sm:text-sm
+          rounded-md
+        "
+        placeholder="Например DOGE"
+      />
+    </div>
+    <coin-suggestions
+      :searchValue="modelValue"
+      @select="onSuggestionClick"
+      @loaded="$emit('loaded')"
+    />
+    <div v-if="error" class="text-sm text-red-600">
+      Такой тикер уже добавлен
+    </div>
+  </div>
+  <button
+    @click="addTicker"
+    type="button"
+    class="
+      my-4
+      inline-flex
+      items-center
+      py-2
+      px-4
+      border border-transparent
+      shadow-sm
+      text-sm
+      leading-4
+      font-medium
+      rounded-full
+      text-white
+      bg-gray-600
+      hover:bg-gray-700
+      transition-colors
+      duration-300
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500
+    "
+  >
+    <add-icon />
+    Добавить
+  </button>
+</template>
+<script>
+import CoinSuggestions from './CoinSuggestions.vue';
+import AddIcon from './AddIcon.vue';
+
+export default {
+  props: {
+    error: String || null,
+    addTicker: Function,
+    modelValue: String,
+  },
+  emits: {
+    'update:modelValue': (v) => typeof v === 'string',
+    blur: null,
+    loaded: null,
+  },
+  components: {
+    CoinSuggestions,
+    AddIcon,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onSuggestionClick(ticker) {
+      this.$emit('update:modelValue', ticker);
+    },
+  },
+};
+</script>
--- src/App.vue
+++ src/App.vue
@@ -39,82 +39,13 @@
 
     <div class="container">
       <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
-            >
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                @keydown.enter="addTicker"
-                @blur="clearError"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="
-                  block
-                  w-full
-                  pr-10
-                  border-gray-300
-                  text-gray-900
-                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
-                  sm:text-sm
-                  rounded-md
-                "
-                placeholder="Например DOGE"
-              />
-            </div>
-            <CoinSuggestions
-              :searchValue="ticker"
-              @select="onSuggestionClick"
-              @loaded="coinlistLoading = false"
-            />
-            <div v-if="error" class="text-sm text-red-600">
-              Такой тикер уже добавлен
-            </div>
-          </div>
-        </div>
-        <button
-          @click="addTicker"
-          type="button"
-          class="
-            my-4
-            inline-flex
-            items-center
-            py-2
-            px-4
-            border border-transparent
-            shadow-sm
-            text-sm
-            leading-4
-            font-medium
-            rounded-full
-            text-white
-            bg-gray-600
-            hover:bg-gray-700
-            transition-colors
-            duration-300
-            focus:outline-none
-            focus:ring-2
-            focus:ring-offset-2
-            focus:ring-gray-500
-          "
-        >
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
+        <add-ticker
+          v-model="ticker"
+          :addTicker="addTicker"
+          :error="error"
+          @loaded="coinlistLoading = false"
+          @blur="clearError"
+        />
         <div class="flex items-center">
           <span>Фильтр:</span>
           <input
@@ -165,7 +96,7 @@
       <template v-if="paginatedTickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <AppTicker
+          <app-ticker
             v-for="t in paginatedTickers"
             :key="t.name"
             :price="formatPrice(t.price)"
@@ -178,7 +109,7 @@
         </dl>
       </template>
       <hr class="w-full border-t border-gray-600 my-4" />
-      <PriceBars
+      <price-bars
         v-if="selected"
         :title="`${selected.name} - USD`"
         :prices="graph"
@@ -189,9 +120,9 @@
 </template>
 
 <script>
+import AddTicker from './AddTicker.vue';
 import AppTicker from './AppTicker.vue';
 import PriceBars from './PriceBars.vue';
-import CoinSuggestions from './CoinSuggestions.vue';
 import { nextTick } from '@vue/runtime-core';
 import { subscribeToTicker, unsubscribeFromTicker } from './api/api';
 import { PRICE_UPDATE, PRICE_ERROR } from './api/constants';
@@ -218,9 +149,9 @@ export default {
     };
   },
   components: {
+    AddTicker,
     AppTicker,
     PriceBars,
-    CoinSuggestions,
   },
   mounted() {
     this.getSavedTickets();
